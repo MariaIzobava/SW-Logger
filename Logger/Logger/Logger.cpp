6adde2ff380fca9b6f4635ba040aff962ced928c
m.izobava <maryia.izobava@solarwinds.com>
2018-11-19 10:01:50 +0300

    Add alternative method with tail recursion.

--- Logger/Logger/Logger.cpp
+++ Logger/Logger/Logger.cpp
@@ -7,9 +7,29 @@
 #include <string>
 #include <sstream>
 
+void concatenate_worker(std::stringstream &ss)
+{}
+
+template <typename First, typename ...Args>
+void concatenate_worker(std::stringstream &ss, First&& first, Args&&... args)
+{
+	ss << std::forward<First>(first);
+	concatenate_worker(ss, std::forward<Args>(args)...);
+}
+
+template <typename ...Args>
+std::string concatenate(Args&& ...args)
+{
+	std::stringstream ss;
+
+	concatenate_worker(ss, std::forward<Args>(args)...);
+
+	return ss.str();
+}
+
 template <class TErr>
-void screen_logger(TErr error, std::string result) {
-	std::cout << "log_level:: " << error << " -" << result;
+void screen_logger(TErr error, std::string message) {
+	std::cout << "log_level:: " << error << " -" << message;
 }
 
 std::string message_concat() {
@@ -25,7 +45,8 @@ std::string message_concat(First first, Args... args) {
 
 template<class TErr, typename ...Args>
 void log(void (*output_func)(TErr, std::string), TErr error, Args... args) {
-	std::string result = message_concat(std::forward<Args>(args)...);
+	//std::string result = message_concat(std::forward<Args>(args)...);
+	std::string result = concatenate(std::forward<Args>(args)...);
 	output_func(error, result);
 }
 
@@ -35,17 +56,20 @@ int main()
 
 	log(screen_logger, 
 					45, 
-					"Ip address", 
-					"10.0.0.10", 
-					"has been blocked, port", 
-					80);
+					" Ip address", 
+					" 10.0.0.10", 
+					" has been blocked, port ", 
+					80,
+					"\n");
 	log(screen_logger,
 		"UNKNOWN_ERROR",
 		"Exit code",
-		13.13);
+		13.13,
+		"\n");
 
 	log(screen_logger,
-		false
+		false,
+		"\n"
 		);
 
 	log(screen_logger,
@@ -56,7 +80,8 @@ int main()
 		'O',
 		'R',
 		'-',
-		"1101 1110 1010 1101 1011 1110 1010 1111"
+		"1101 1110 1010 1101 1011 1110 1010 1111",
+		"\n"
 	);
 	getchar();
 }
